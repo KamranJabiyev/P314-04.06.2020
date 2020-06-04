6399f8c88142bc964306a9ddaf98b2e08455a16c
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-06-04 21:08:46 +0400

    Add files via upload

---
+++ Course/Course/Group.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Group
+    {
+        public string Name { get; set; }
+        public int MaxStudentCount { get; set; }
+
+        private Student[] students;
+
+        public readonly int Id;
+        private static int count = 0;
+
+        public Group()
+        {
+            count++;
+            Id = count;
+            students = new Student[0];
+        }
+
+        public Group(string name,int maxStuCount):this()
+        {
+            Name = name;
+            MaxStudentCount = maxStuCount;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public void AddStudent(Student newStu)
+        {
+            if (students.Length < MaxStudentCount)
+            {
+                foreach (Student item in students)
+                {
+                    if (item.Id == newStu.Id)
+                    {
+                        Console.WriteLine($"{newStu.Name} adli telebeni artiq elave etmisiniz");
+                        return;
+                    }
+                }
+                Array.Resize(ref students,students.Length+1);
+                students[students.Length - 1] = newStu;
+                //Console.WriteLine("Telebe elave olundu");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} grupu doludur.");
+            }
+        }
+
+        public  void ShowStudentList()
+        {
+            Console.WriteLine($"{Name} grupunda olan telebelerin siyahisi:");
+            foreach (Student item in students)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void IsExist(string name,string surname)
+        {
+            foreach (Student item in students)
+            {
+                if(item.Name==name && item.Surname == surname)
+                {
+                    Console.WriteLine($"{Name} qrupunda {name} {surname} adli telebe var");
+                    return;
+                }
+            }
+            Console.WriteLine($"{Name} qrupunda {name} {surname} adli telebe YOXDUR!!!");
+        }
+    }
+}
---
+++ Course/Course/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Javidan", "Mediyev");
+            Student s2 = new Student("Seynur", "Memmedov");
+            Student s3 = new Student("Osman", "Ismayilov");
+            Student s4 = new Student("Zahid", "Qasimli");
+            Student s5 = new Student("Kamil", "Memmedov");
+
+            Group p314 = new Group("P314",2);
+            p314.AddStudent(s1);
+            //p314.AddStudent(s1);
+            p314.AddStudent(s3);
+            Group p116 = new Group("P116", 18);
+            p116.AddStudent(s2);
+            p116.AddStudent(s4);
+            p116.AddStudent(s5);
+
+            p314.IsExist("Javidan", "Mediyev");
+            //p314.ShowStudentList();
+            //p116.ShowStudentList();
+        }
+    }
+}
